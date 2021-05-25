be582fb0694785d811eb2da9e85baab7b6f591b2
Kyle B <kyleberman92@gmail.com>
2021-05-25 03:02:33 -0700

    added 5 day forecast and localstorage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+const shortDate = (dt) => {
+  let dateObject = new Date(dt * 1000)
+  let humanDateFormat = dateObject.toLocaleString()
+  let sdate = dateObject.toLocaleString("en-US", { weekday: "long" }) + ", " + dateObject.toLocaleString("en-US", { month: "numeric" }) + "/" + dateObject.toLocaleString("en-US", { day: "numeric" }) + "/" + dateObject.toLocaleString("en-US", { year: "numeric" })
+  return sdate
+}
+
 document.getElementById('weatherSearch').addEventListener('click', event => {
   event.preventDefault()
   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('citySearch').value}&units=imperial&appid=68d634195147f679bc3416e5b0e9d434`)
@@ -9,38 +16,44 @@ document.getElementById('weatherSearch').addEventListener('click', event => {
       document.getElementById('temp').innerHTML = `${weather.main.temp}`
       document.getElementById('hum').innerHTML = `${weather.main.humidity}`
       document.getElementById('wndSpd').innerHTML = `${weather.wind.speed}`
-      // document.getElementById('uvInd').innerHTML = `${weather.name}`
       document.getElementById('date').innerHTML = Date()
+//gets 5 day forecast
+      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&units=imperial&appid=68d634195147f679bc3416e5b0e9d434`)
+        .then(resp => {
+          let weatherc = resp.data.current
+          let future = resp.data.daily
+          let resultName = res.data.name
 
-      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&exclude=hourly,daily&appid=68d634195147f679bc3416e5b0e9d434`)
-        .then(res => {
-          let weather = res.data.current
-          console.log(res)
-          document.getElementById('uvInd').innerHTML = `${weather.uvi}`
-          if (weather.uvi >= 0 && weather.uvi < 3) {
-            document.getElementById("uvInd").classList.add("bg-success")
-          } else if (weather.uvi >= 3 && weather.uvi < 6) {
-            document.getElementById("uvInd").classList.add("bg-warning")
-          } else if (weather.uvi >= 6 && weather.uvi < 9) {
-            document.getElementById("uvInd").classList.add("bg-danger")
-          } else {
-            document.getElementById("uvInd").classList.add("bg-dark")
+          let history = JSON.parse(localStorage.getItem('history')) || []
+          history.push({ resultName })
+          localStorage.setItem('history', JSON.stringify(history))
+          console.log(history)
+          console.log(future)
+          for (let i = 1; i < 7; i++) {
+            document.getElementById(`slot-${i}`).innerHTML = `
+                  <div class="card">
+                  <div class="card-body">
+                  <h5 class="card-title">${shortDate(future[i].dt)}</h5>
+                 
+                  <h6 class="card-text">Max: ${future[i].temp.max} &degF</h6>
+                  <h6 class="card-text">Min: ${future[i].temp.min} &degF</h6>
+                  <h6 class="card-text">Humidity: ${future[i].humidity}%</h6>
+                  </div>
+                  </div>
+                  `
+
+            console.log(res)
+            document.getElementById('uvInd').innerHTML = `${weatherc.uvi}`
+            if (weatherc.uvi >= 0 && weatherc.uvi < 3) {
+              document.getElementById("uvInd").classList.add("bg-success")
+            } else if (weatherc.uvi >= 3 && weatherc.uvi < 6) {
+              document.getElementById("uvInd").classList.add("bg-warning")
+            } else if (weatherc.uvi >= 6 && weatherc.uvi < 9) {
+              document.getElementById("uvInd").classList.add("bg-danger")
+            } else {
+              document.getElementById("uvInd").classList.add("bg-dark")
+            }
           }
         })
-        .catch(err => console.error(err))
-    }
-    )
+      })
 })
-
-// gets 5 day forecast
-document.getElementById('weatherSearch').addEventListener('click', event => {
-  event.preventDefault()
-  axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${document.getElementById('citySearch').value}&units=imperial&appid=68d634195147f679bc3416e5b0e9d434`)
-    .then(res => {
-      let weather = res.data.data
-      console.log(res)
-      for (let i = 5; i < citySearch.length; i++) {
-        console.log()
-      }
-    })
-  })
